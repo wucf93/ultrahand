a543a29ab8bb6fac14b216e4d10b9254c9ad0480
fan <1004143951@qq.com>
2023-06-15 00:21:33 +0800

    Revert "feat: theme"
    This reverts commit 0377c314cf5538c575009b64c643983462931e66.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// router
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-// antd
 import { ConfigProvider } from "antd";
+import { router } from "./router";
 import zhCN from "antd/locale/zh_CN";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{ token: { colorPrimary: "#1d93ab" } }}
+    >
       <RouterProvider router={router} />
     </ConfigProvider>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//antd 主题
-import { theme } from 'antd';
-const mapToken = theme.defaultAlgorithm(theme.defaultSeed)
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  resolve: { alias: { "@": "/src" } },
+  resolve: { alias: { "@": "/src", "@public": "/public" } },
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: { modifyVars: mapToken },
-      javascriptEnabled: true
-    }
-  }
 });
