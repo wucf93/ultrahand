0377c314cf5538c575009b64c643983462931e66
wcf93 <1004143951@qq.com>
2023-06-15 00:13:49 +0800

    feat: theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+//antd 主题
+import { theme } from 'antd';
+const mapToken = theme.defaultAlgorithm(theme.defaultSeed)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  resolve: { alias: { "@": "/src", "@public": "/public" } },
+  resolve: { alias: { "@": "/src" } },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: { modifyVars: mapToken },
+      javascriptEnabled: true
+    }
+  }
 });
